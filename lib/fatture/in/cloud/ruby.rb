7b4ac7dec3aa6b15d4fbc45ec159860f4a8ed139
Andrea Vanini <andrea.vanini@uidu.org>
2019-02-11 15:48:16 +0100

    Gem init

---
+++ lib/fatture/in/cloud/ruby.rb
@@ -0,0 +1,12 @@
+require "fatture/in/cloud/ruby/version"
+
+module Fatture
+  module In
+    module Cloud
+      module Ruby
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fatture/in/cloud/ruby/version.rb
@@ -0,0 +1,9 @@
+module Fatture
+  module In
+    module Cloud
+      module Ruby
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
