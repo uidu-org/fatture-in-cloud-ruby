d74faeed6a8f7e8e6df9c00a73d4954738e9d38c
Andrea Vanini <andrea.vanini@uidu.org>
2019-02-11 15:50:55 +0100

    Naming

---
+++ lib/fatture_in_cloud_ruby.rb
@@ -0,0 +1,6 @@
+require "fatture_in_cloud_ruby/version"
+
+module FattureInCloudRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fatture_in_cloud_ruby/version.rb
@@ -0,0 +1,3 @@
+module FattureInCloudRuby
+  VERSION = "0.1.0"
+end
