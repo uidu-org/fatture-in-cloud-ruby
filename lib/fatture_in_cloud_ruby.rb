9573dc6eb307ffe9fd957b9f6249bbb4f85e1734
Andrea Vanini <andrea.vanini@uidu.org>
2020-01-02 17:30:27 +0100

    Release 0.1.3

--- lib/fatture_in_cloud_ruby.rb
+++ lib/fatture_in_cloud_ruby.rb
@@ -1,11 +1,9 @@
 require 'api_struct'
 require 'dry-configurable'
 
-# require 'fatture_in_cloud_ruby/settings'
-require 'fatture_in_cloud_ruby/api_struct_settings'
-
-require "fatture_in_cloud_ruby/version"
-require "fatture_in_cloud_ruby/clients/corrispettivi"
+require_relative 'api_struct_settings'
+require_relative "version"
+require_relative "clients/corrispettivi"
 
 module FattureInCloudRuby
   extend Dry::Configurable
--- lib/fatture_in_cloud_ruby/version.rb
+++ lib/fatture_in_cloud_ruby/version.rb
@@ -1,3 +1,3 @@
 module FattureInCloudRuby
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
