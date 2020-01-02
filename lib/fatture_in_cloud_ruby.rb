da52e3c19e503cdb3f628d3e3cfa2379f44ade0d
Andrea Vanini <andrea.vanini@uidu.org>
2020-01-02 17:57:36 +0100

    Settings are back

--- lib/fatture_in_cloud_ruby.rb
+++ lib/fatture_in_cloud_ruby.rb
@@ -1,15 +1,11 @@
 require 'api_struct'
 require 'dry-configurable'
 
-require_relative "fatture_in_cloud_ruby/version"
+require_relative 'fatture_in_cloud_ruby/version'
+require_relative 'fatture_in_cloud_ruby/settings'
 require_relative 'fatture_in_cloud_ruby/api_struct_settings'
-require_relative "fatture_in_cloud_ruby/clients/corrispettivi"
+require_relative 'fatture_in_cloud_ruby/clients/corrispettivi'
 
 module FattureInCloudRuby
-  extend Dry::Configurable
-
   class Error < StandardError; end
-  # Your code goes here...
-  setting :api_key
-  setting :api_uid
 end
